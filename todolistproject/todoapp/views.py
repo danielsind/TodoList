2a6fd17c6d49734ce87f51a09f2e4029e2f758c6
danielsind <danielsindet@gmail.com>
2023-05-15 23:39:51 +0300

    Edited the import from .models import Task

--- todolistproject/todoapp/views.py
+++ todolistproject/todoapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
-from . import Task
+from .models import Task
 # Create your views here.
 class TaskList(ListView):
     model = Task

ca8817748250368d67c3036b99043010842fc319
danielsind <danielsindet@gmail.com>
2023-05-15 23:28:52 +0300

    Created the class TaskList in views.py for the database to be represented and then Imported the class TaskList from views.py to urls.py to help view the database,

--- todolistproject/todoapp/views.py
+++ todolistproject/todoapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.views.generic.list import ListView
+from . import Task
 # Create your views here.
-def home(request):
-    return HttpResponse('Working')
+class TaskList(ListView):
+    model = Task

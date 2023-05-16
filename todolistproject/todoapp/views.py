d5d98aad2588aa6b770a211a6d4f41c413a9d78a
danielsind <danielsindet@gmail.com>
2023-05-16 21:09:01 +0300

    Created a task_deatail.html file for the task detail view

--- todolistproject/todoapp/views.py
+++ todolistproject/todoapp/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Task
 # Create your views here.
 class TaskList(ListView):
     model = Task
     context_object_name = 'tasks'
+
+class TaskDetail(DetailView):
+    model = Task

3dfc7c25df48a8ccc30ef64a4cbec2f7e8173bab
danielsind <danielsindet@gmail.com>
2023-05-17 20:54:26 +0300

    Added the comma, at the path to correct the error

--- todolistproject/todoapp/urls.py
+++ todolistproject/todoapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, UserLoginView
 
 
 urlpatterns = [
     path('', TaskList.as_view(), name='task' ),
+    path('login/', UserLoginView.as_view(), name = 'login'),
     path('task-create', TaskCreate.as_view(), name='task-create'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task_detail'),
     path('task-update/<int:pk>/', TaskUpdate.as_view(), name = 'task-update'),

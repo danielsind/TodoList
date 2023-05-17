0c95fbdc3fa33c29542506aed9356ec8621a8a60
danielsind <danielsindet@gmail.com>
2023-05-17 21:29:12 +0300

    Imported LogoutView on url.py and added a link to the path logout in task_list.html

--- todolistproject/todoapp/urls.py
+++ todolistproject/todoapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, UserLoginView
+from django.contrib.auth.views import LogoutView
 
 
 urlpatterns = [
@@ -7,6 +8,7 @@ urlpatterns = [
     path('login/', UserLoginView.as_view(), name = 'login'),
     path('task-create', TaskCreate.as_view(), name='task-create'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task_detail'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('task-update/<int:pk>/', TaskUpdate.as_view(), name = 'task-update'),
     path('task-delete/<int:pk>/', TaskDelete.as_view(), name = 'task-delete')
 ]
